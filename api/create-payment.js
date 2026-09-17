222582f90d4c569ed9262f23e459c59eede492ee
emilescheijen-collab <emile.scheijen@gmail.com>
2026-09-17 02:02:00 +0200

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -27,8 +27,8 @@ currency: 'EUR',
 value: bedrag
 },
 description: `Koo-sjiete RKVV Vijlen - ${kavels.length} kavel(s)`,
-redirectUrl: `${req.headers.origin}/?betaling=terug`,
-webhookUrl: `${req.headers.origin}/api/mollie-webhook`,
+redirectUrl: `https://koosjiete-2026-rkvv-vijlen.vercel.app/?betaling=terug`,
+webhookUrl: `https://koosjiete-2026-rkvv-vijlen.vercel.app/api/mollie-webhook`,
 metadata: {
 bestellingId: bestellingId,
 kavels: kavels
